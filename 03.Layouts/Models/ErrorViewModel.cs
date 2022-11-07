f0c4e1f794a5eb1b45dbb9e65fd89c4edffe6fd1
Ümit KARAÇİVİ <umit.karacivi@outlook.com>
2022-11-07 21:58:11 +0300

    Some additions

---
+++ 03.Layouts/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace _03.Layouts.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ 04.LayoutsBolumlu/Views/Shared/_LayoutBolumlu.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace _04.LayoutsBolumlu.Views.Shared
+{
+    public class _LayoutBolumluModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
