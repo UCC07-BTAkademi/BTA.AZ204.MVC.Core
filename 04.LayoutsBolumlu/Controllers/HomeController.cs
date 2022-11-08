252809a701ac606e701291725dcff8b19f2f3c8b
Ümit KARAÇİVİ <umit.karacivi@outlook.com>
2022-11-08 16:53:01 +0300

    some changes

--- 04.LayoutsBolumlu/Controllers/HomeController.cs
+++ 04.LayoutsBolumlu/Controllers/HomeController.cs
@@ -33,6 +33,7 @@ namespace _04.LayoutsBolumlu.Controllers
             return View();
         }
 
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
