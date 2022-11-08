363bf152f42b339dc59d302195787bc9f406c647
Ümit KARAÇİVİ <umit.karacivi@outlook.com>
2022-11-08 20:48:25 +0300

    ViewBagViewData eklendi

---
+++ 06.ViewBagViewData/Controllers/StudentController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Net.Cache;
+
+using _06.ViewBagViewData.Models;
+
+namespace _06.ViewBagViewData.Controllers
+{
+    public class StudentController : Controller
+    {
+        IList<Student> studentList = new List<Student>()
+        {
+                    new Student(){ StudentId=1, StudentName="Ümit", Age = 58 },
+                    new Student(){ StudentId=2, StudentName="Nurgül", Age = 54 },
+                    new Student(){ StudentId=3, StudentName="Doğa", Age = 22 },
+                    new Student(){ StudentId=4, StudentName="Barış", Age = 50 },
+                    new Student(){ StudentId=5, StudentName="Ayşegül", Age = 50 }
+        };
+
+        public IActionResult Index()
+        {
+            ViewBag.TotalStudents = studentList.Count; 
+            
+            ViewData["students"]=studentList;   
+
+
+
+            return View();
+        }
+    }
+}
---
+++ 06.ViewBagViewData/Models/Student.cs
@@ -0,0 +1,11 @@
+﻿namespace _06.ViewBagViewData.Models
+{
+    public class Student
+    {
+        public int StudentId { get; set; }
+
+        public string StudentName { get; set; }
+
+        public int Age { get; set; }
+    }
+}
