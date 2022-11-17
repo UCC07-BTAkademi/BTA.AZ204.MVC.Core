d6cdbecd03bc426fd1b0412dd0f40fa7ab0a126a
Ümit KARAÇİVİ <umit.karacivi@outlook.com>
2022-11-17 22:27:53 +0300

    DBFirst_EF eklendi

---
+++ DBFirst_EF/Program.cs
@@ -0,0 +1,29 @@
+﻿using DBFirst_EF.Models;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+// Database connection string değerinin appsettings.json dosyasından öğrenilmesi.
+builder.Services.AddDbContext<NorthwindContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("ConnStr")));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+}
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
